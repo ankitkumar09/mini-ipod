f9d42f2475b2b88544570ad4e5acdd5f817c3388
Ankit kumar <46224792+ankitkumar09@users.noreply.github.com>
2021-02-18 01:07:46 +0530

    ccreated button and styled it

---
+++ src/Button.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Buttons = (props) =>
+{
+    return (
+        <div className="buttons-container">
+            
+            <button className="center-circle">
+                <h2>Select</h2>
+            </button>
+
+            <button className="menu-button">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <button className="left-button">
+                <i class="fas fa-backward"></i>
+            </button>
+            
+            <button className="right-button">
+                <i class="fas fa-forward"></i>
+            </button>
+
+            <button className="play-pause">
+                <i class="fas fa-play"></i> /<i class="fas fa-pause"></i>
+            </button>
+        </div>
+    );
+}
+
+export default Buttons;
